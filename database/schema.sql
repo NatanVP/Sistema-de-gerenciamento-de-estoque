9112e3eb5d18b43a157786f6af694f4e95eb8149
Natan Vieira <natanvieirapasquantonio@gmail.com>
2025-09-12 10:20:46 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -17,14 +17,3 @@ CREATE TABLE produtos (
     quantidade INT DEFAULT 0,
     preco_unitario DECIMAL(10,2)
 );
-
-CREATE TABLE movimentacoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    produto_id INT,
-    usuario_id INT,
-    tipo ENUM('entrada','saida'),
-    quantidade INT NOT NULL,
-    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (produto_id) REFERENCES produtos(id),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
